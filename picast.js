cca44180e5aa53b5181c610aa839bf1bdedeb8f9
yodaarjun <asing203@eq.edu.au>
2016-09-18 20:00:37 +1000

    Update picast.js

--- picast.js
+++ picast.js
@@ -7,9 +7,9 @@ app.get('/', function (req, res) {
         res.send('Welcome to PiCAST 3! In the URL, type what you want to do...');
 });
 
-app.get('/yt-stream/:url', function (req, res) {
+app.get('/:url', function (req, res) {
         res.send('Streaming YouTube Video...');
-        exec("livestreamer --player=mplayer https://www.youtube.com/watch?v=" + req.params.url + " best");
+        exec("livestreamer --player=mplayer req.params.url + " best");
 });
 
 // Setup PiCAST Server
